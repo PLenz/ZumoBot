fc2926ac96e5f06dcf10a5ca5474213e2e22f6c8
Patrick Lukas Lenz <patrick.lenz@mni.thm.de>
2017-03-09 23:25:03 +0100

    index.html states - removed pictures

--- Web/public/js/zumo.js
+++ Web/public/js/zumo.js
@@ -1,6 +1,3 @@
-  /**
-   * @see https://addyosmani.com/resources/essentialjsdesignpatterns/book/#revealingmodulepatternjavascript
-   */
 ;window.zumo = (function(){
     var connection = new WebSocket('ws://' + location.hostname +':8082');
 
@@ -15,12 +12,19 @@
             console.log(message);
         }
 
-    function move(x, xneg, y, yneg){
-        send({
-                mode : 1,
-                data : [x, xneg, y, yneg]
-        });
-    }
+        function move(x, xneg, y, yneg){
+            send({
+                    mode : 1,
+                    data : [x, xneg, y, yneg]
+            });
+        }
+
+        function mode(state){
+            send({
+                    mode : 0,
+                    data : [state]
+            });
+        }
 
     return {
             move : move,
