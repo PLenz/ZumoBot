6c5f59c210bdbb14f73dab514a4fb9b377da384e
Patrick Lukas Lenz <patrick.lenz@mni.thm.de>
2017-03-10 14:39:22 +0100

    buzzer

--- Arduino/bot/bot.ino
+++ Arduino/bot/bot.ino
@@ -1,11 +1,13 @@
 #include<Wire.h>
 #include<ZumoMotors.h>
+#include <ZumoBuzzer.h>
 
 const int MAX_SPEED = 400;
 const byte LED = 13;
 const int deadZone = 10;
 byte state = 0;
 ZumoMotors motors;
+ZumoBuzzer buzzer;
 
 void setup() {
   pinMode(LED, OUTPUT);
@@ -20,9 +22,9 @@ void loop() {
 }
 
 void clear(int count) {
-  for(int i = 0; i < count; i++) {
-      Wire.read();
-    }
+  for (int i = 0; i < count; i++) {
+    Wire.read();
+  }
 }
 
 void receiveEvent(int count) {
@@ -31,6 +33,18 @@ void receiveEvent(int count) {
   if (mode == 0) {
     if (count == 1) {
       int state = Wire.read();
+      count--;
+      switch (state)
+      {
+        case 0:
+          motors.setLeftSpeed(0);
+          motors.setRightSpeed(0);
+          break;
+        case 1:
+          buzzer.play("L16 cdegreg4");
+          while (buzzer.isPlaying());
+          break;
+      }
     }
   } else if (mode == 1) {
     if (count == 4) {
@@ -44,54 +58,56 @@ void receiveEvent(int count) {
       int y_neg = Wire.read();
       count--;
 
-      int throttle, direc = 0;
+      if (state == 1) {
+        int throttle, direc = 0;
 
-      if (x_neg == 1)
-        direc = x * -1;
-      else
-        direc = x;
-      direc = direc * -1;
-
-      if (y_neg == 1)
-        throttle = y * -1;
-      else
-        throttle = y;
-
-      int leftMotor, rightMotor = 0;
-      float leftMotorScale, rightMotorScale, maxMotorScale = 0;
-
-      leftMotor = throttle + direc;
-      rightMotor = throttle - direc;
-
-      leftMotorScale = leftMotor / 400.0;
-      leftMotorScale = abs(leftMotorScale);
-      rightMotorScale = rightMotor / 400.0;
-      rightMotorScale = abs(rightMotorScale);
-
-      maxMotorScale = max(leftMotorScale, rightMotorScale);
-      maxMotorScale = max(1, maxMotorScale);
-
-      int leftMotorScaled = 0;
-      int rightMotorScaled = 0;
-
-      leftMotorScaled = constrain(leftMotor / maxMotorScale, -400, 400);
-      rightMotorScaled = constrain(rightMotor / maxMotorScale, -400, 400);
-
-      if (leftMotorScaled >= 240 && rightMotorScaled >= 240) {
-        motors.setLeftSpeed(MAX_SPEED); 
-        motors.setRightSpeed(MAX_SPEED);
-      } else {
-        if (abs(leftMotorScaled) > deadZone)
-          motors.setLeftSpeed(leftMotorScaled);
+        if (x_neg == 1)
+          direc = x * -1;
         else
-          motors.setLeftSpeed(0);
+          direc = x;
+        direc = direc * -1;
 
-        if (abs(rightMotorScaled) > deadZone)
-          motors.setRightSpeed(rightMotorScaled);
+        if (y_neg == 1)
+          throttle = y * -1;
         else
-          motors.setRightSpeed(0);
+          throttle = y;
+
+        int leftMotor, rightMotor = 0;
+        float leftMotorScale, rightMotorScale, maxMotorScale = 0;
+
+        leftMotor = throttle + direc;
+        rightMotor = throttle - direc;
+
+        leftMotorScale = leftMotor / 400.0;
+        leftMotorScale = abs(leftMotorScale);
+        rightMotorScale = rightMotor / 400.0;
+        rightMotorScale = abs(rightMotorScale);
+
+        maxMotorScale = max(leftMotorScale, rightMotorScale);
+        maxMotorScale = max(1, maxMotorScale);
+
+        int leftMotorScaled = 0;
+        int rightMotorScaled = 0;
+
+        leftMotorScaled = constrain(leftMotor / maxMotorScale, -400, 400);
+        rightMotorScaled = constrain(rightMotor / maxMotorScale, -400, 400);
+
+        if (leftMotorScaled >= 240 && rightMotorScaled >= 240) {
+          motors.setLeftSpeed(MAX_SPEED);
+          motors.setRightSpeed(MAX_SPEED);
+        } else {
+          if (abs(leftMotorScaled) > deadZone)
+            motors.setLeftSpeed(leftMotorScaled);
+          else
+            motors.setLeftSpeed(0);
+
+          if (abs(rightMotorScaled) > deadZone)
+            motors.setRightSpeed(rightMotorScaled);
+          else
+            motors.setRightSpeed(0);
+        }
+        delay(10);
       }
-      delay(10);
     }
   }
   if (count > 0)
